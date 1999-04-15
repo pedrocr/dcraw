d793729a41d4c1635b7a62eb9d7fa7735ba93acf
Dave Coffin <dcoffin@cybercom.net>
1999-04-15 19:57:55 +0000

    Replaced "60000" with a macro.

--- dcraw.c
+++ dcraw.c
@@ -287,7 +287,7 @@ write_ppm(char *fname)
    Set the maximum to the 96th percentile
 */
   for (val=512, total=0; --val; )
-    if ((total+=histo[val]) > 60000) break;
+    if ((total+=histo[val]) > (int)(W*H*0.11)) break;
   max = val << 10;
   max2 = max*max;
 
