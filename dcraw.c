d1964500757342d14ed3cfb0a1fe645caf6ea37e
Dave Coffin <dcoffin@cybercom.net>
2002-11-24 15:00:57 +0000

    Fixed segfault caused by "crw -d -4"

--- dcraw.c
+++ dcraw.c
@@ -1844,7 +1844,7 @@ void write_png(FILE *ofp)
       for (c=0; c < 3; c++) {
 	val = rgb[c] * bright;
 	if (val > 0xffff) val=0xffff;
-	png[x-1][c] = val;
+	png[x-trim][c] = val;
       }
     }
     png_write_row(png_ptr, (png_bytep) png);
@@ -1879,7 +1879,7 @@ int main(int argc, char **argv)
   if (argc == 1)
   {
     fprintf (stderr,
-    "\nRaw Photo Decoder v3.52"
+    "\nRaw Photo Decoder v3.53"
 #ifdef LJPEG_DECODE
     " with Lossless JPEG support"
 #endif
